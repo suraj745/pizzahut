5d363367d0846f32f6980d2217fd8ac2e9fce11d
suraj arya <surajarya745@gmail.com>
2022-11-25 13:37:36 +0530

    react-template

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <h1 className="heading">Hello</h1>
+        </li>
+        <li>
+          <p>Hello</p>
+        </li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
